daca8ed81bc586e642357bc7cf8559f6cc753992
Charlie Nguyen <charliehoangnguyen@gmail.com>
2020-02-09 20:58:49 -0800

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Charlie on 2/9/20.
+//  Copyright © 2020 Charlie Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
